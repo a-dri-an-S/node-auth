4d999ba319ef2a1b21f13c7bc0ae524351b97b41
a-dri-an-S <adrian.salinas.1986@gmail.com>
2020-12-14 16:25:30 -0800

    changed to npm instead of yarn in heroku-postbuild

--- routes/messages.js
+++ routes/messages.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { verifyToken } = require('../controllers/auth-controller');
 const { seedDB, getMessagesBySender, getUserThreads } = require("../controllers/messages-controller");
 
-// router.get('/', verifyToken, (req, res) => {
+// router.get('/', (req, res) => {
 //     res.json({ 
 //     message: { 
 //         receiver: "5fcd54c4b983b506491ea863",
@@ -11,10 +11,10 @@ const { seedDB, getMessagesBySender, getUserThreads } = require("../controllers/
 //     } 
 // })})
 
-router.get("/", verifyToken, getUserThreads);
+// router.get("/",  getUserThreads);
 
 // GET //
-// router.get("/", getMessagesBySender);
+router.get("/", getMessagesBySender);
 // router.get("/"); // This will return a list of users that the logged in user has ever communicated with
 
 // POST //
